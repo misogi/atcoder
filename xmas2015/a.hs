8dc101929a417ede4616983de8edf1fd4cf4beb3
misogi <dev@misogi.info>
2016-01-01 23:15:30 +0900

    ver 2

---
+++ xmas2015/a.hs
@@ -0,0 +1,28 @@
+
+readInt :: IO Int
+readInt = readLn
+
+readInts :: IO [Int]
+readInts = fmap ((map read) . words) getLine
+
+atVal :: Int -> Int -> Int -> Int
+atVal 0 a c = 1
+atVal t a c = (a * atVal (div t 2) a c) `mod` c
+
+btVal :: Int -> Int -> Int -> Int -> Int
+btVal 0 a b c = 1
+btVal t a b c = (a * (btVal (div t 2) a b c) + b) `mod` c
+
+oneSum :: Int -> Int -> Int -> Int -> Int
+oneSum x at bt c = ( at * x + bt ) `mod` c;
+
+allSum :: Int -> Int -> Int -> Int -> Int -> [Int]
+allSum x at bt c 0 = []
+allSum x at bt c n = (oneSum x at bt c) : (allSum x at bt c (n-1))
+
+main = do
+  n <- readInt
+  [x, t, a, b, c] <- readInts
+  let at = atVal t a c
+  let bt = btVal t a b c
+  putStrLn $ show $ allSum x at bt c n
