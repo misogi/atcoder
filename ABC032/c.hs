24e2a0e79bc3cf9b6cf43c7b55e4403e9dd2bdad
m1sogi <ryudo@misogi.info>
2016-01-12 21:48:06 +0900

    かけ算の値を値として持っておくようにして高速化

--- ABC032/c.hs
+++ ABC032/c.hs
@@ -9,20 +9,20 @@ main = do
 solve :: [Int] -> Int -> Int
 solve xs k
   | 0 `elem` xs = length xs
-  | otherwise = calcMaxLen xs Seq.empty 0 k
+  | otherwise = calcMaxLen xs Seq.empty 0 1 k
 
-calcMaxLen :: [Int] -> Seq.Seq Int -> Int -> Int -> Int
-calcMaxLen [] _ _ _ = 0
-calcMaxLen (x:xs) a maxLength k
-  | k < x     = calcMaxLen xs Seq.empty maxLength k
-  | xam <= k  = max xaLen $ calcMaxLen xs xa maxl k
+calcMaxLen :: [Int] -> Seq.Seq Int -> Int -> Int -> Int -> Int
+calcMaxLen [] _ maxLength _ _ = maxLength
+calcMaxLen (x:xs) a maxLength multiVal k
+  | k < x     = calcMaxLen xs Seq.empty maxLength 1 k
+  | xam <= k  = max xaLen $ calcMaxLen xs xa maxl xam k
   | otherwise =
     let aHead Seq.:< aTail = Seq.viewl a
-    in calcMaxLen (x:xs) aTail maxLength k
-  where xa = (x Seq.<| a)
+    in calcMaxLen (x:xs) aTail maxLength (multiVal `div` aHead) k
+  where xa = (a Seq.|> x)
         xaLen = Seq.length xa
         maxl = max maxLength xaLen
-        xam = foldl1 (*) xa
+        xam = multiVal * x
 
 readInt :: IO Int
 readInt = readLn
