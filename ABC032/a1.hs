9d4f179fb9fdd933388a020d6022592b45f54aa2
misogi <dev@misogi.info>
2016-01-17 21:03:31 +0900

    solve B

--- ABC032/a1.hs
+++ ABC032/a1.hs
@@ -1,1 +1,12 @@
-euclid :: Int -> Int -> Int
+main = do
+  a <- readInt
+  b <- readInt
+  n <- readInt
+  putStrLn . show $ solve a b n
+
+solve :: Int -> Int -> Int -> Int
+solve a b n = (n + lcmAb - 1) `div` lcmAb * lcmAb
+  where lcmAb = lcm a b
+
+readInt :: IO Int
+readInt = readLn
--- ARC047/b.rb
+++ ARC047/b.rb
@@ -1,27 +1,42 @@
+def compare_distance(ans, n, xs, ys)
+  x, y = ans
+  px = (x + y) / 2
+  py = (x - y) / 2
+  dist = nil
+  n.times do |i|
+    dd = (xs[i] - px).abs + (ys[i] - py).abs
+    if dist && dist != dd
+      return false
+    end
+    dist = dd
+  end
+
+  puts "#{px} #{py}"
+  true
+end
+
 n = gets.chomp.to_i
 
-xyms = {}
-xyss = {}
-xyms_ans = []
-xyss_ans = []
+a = []
+b = []
+xs = []
+ys = []
 
 n.times do
   x, y = gets.chomp.split(' ').map(&:to_i)
-  xym = x - y
-  xys = x + y
-  if xyms[xym]
-    xyms_ans << xym
-  else
-    xyms[xym] = 1
-  end
-
-  if xyss[xys]
-    xyss_ans << xys
-  else
-    xyss[xys] = 1
-  end
-  puts xym, xys
+  xs << x
+  ys << y
+  a << x + y
+  b << x - y
 end
 
-puts xyss_ans
-puts xyms_ans
+amax = a.max
+amin = a.min
+bmax = b.max
+bmin = b.min
+d = [amax-amin, bmax-bmin].max.div(2)
+answers = [[amin+d, bmin+d], [amin+d, bmax-d], [amax-d, bmin+d], [amax-d, bmax-d]]
+
+answers.each do |ans|
+  exit if compare_distance(ans, n, xs, ys)
+end
