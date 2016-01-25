b708929eaf410048e0389e5fa8a884c1a0a51470
misogi <dev@misogi.info>
2016-01-25 10:38:31 +0900

    D問題のvに制限がある場合

--- ABC032/d.cpp
+++ ABC032/d.cpp
@@ -5,66 +5,92 @@
 using namespace std;
 
 int main() {
-  uint64_t n, w;
-  cin >> n >> w;
-  vector<uint> weights(n), values(n);
-  for (auto i=0;i<n;i++) {
-    cin >> values[i] >> weights[i];
-  }
-
-  map<uint64_t, uint64_t> ms, second_half;
-  int halfn = n / 2;
-  for (auto i=0;i<(1 << halfn);i++) {
-    uint64_t w_sum = 0, v_sum = 0;
-    for (auto j=0;j<halfn;j++) {
-      if ((1 << j) & i){
-        w_sum += weights[j];
-        v_sum += values[j];
-      }
-    }
-
-    if (w_sum <= w) {
-      if (ms.count(w_sum) == 1) {
-        if (ms[w_sum] < v_sum) {
-          ms[w_sum] = v_sum;
+        uint64_t n, w;
+        cin >> n >> w;
+        vector<uint> weights(n), values(n);
+        for (auto i=0; i<n; i++) {
+                cin >> values[i] >> weights[i];
         }
-      } else {
-        ms[w_sum] = v_sum;
-      }
-    }
-  }
 
-  for (auto i=0;i<(1 << (n-halfn));i++) {
-    uint64_t w_sum = 0, v_sum = 0;
-    for (auto j=0;j<(n-halfn);j++) {
-      if ((1 << j) & i) {
-        v_sum += values[halfn+j];
-        w_sum += weights[halfn+j];
-      }
-    }
+        if (n <= 30) {
+                map<uint64_t, uint64_t> ms, second_half;
+                int halfn = n / 2;
+                for (auto i=0; i<(1 << halfn); i++) {
+                        uint64_t w_sum = 0, v_sum = 0;
+                        for (auto j=0; j<halfn; j++) {
+                                if ((1 << j) & i) {
+                                        w_sum += weights[j];
+                                        v_sum += values[j];
+                                }
+                        }
 
-    if (w_sum <= w) {
-      if (second_half.count(w_sum) == 1) {
-        if (second_half[w_sum] < v_sum) {
-          second_half[w_sum] = v_sum;
+                        if (w_sum <= w) {
+                                if (ms.count(w_sum) == 1) {
+                                        if (ms[w_sum] < v_sum) {
+                                                ms[w_sum] = v_sum;
+                                        }
+                                } else {
+                                        ms[w_sum] = v_sum;
+                                }
+                        }
+                }
+
+                for (auto i=0; i<(1 << (n-halfn)); i++) {
+                        uint64_t w_sum = 0, v_sum = 0;
+                        for (auto j=0; j<(n-halfn); j++) {
+                                if ((1 << j) & i) {
+                                        v_sum += values[halfn+j];
+                                        w_sum += weights[halfn+j];
+                                }
+                        }
+
+                        if (w_sum <= w) {
+                                if (second_half.count(w_sum) == 1) {
+                                        if (second_half[w_sum] < v_sum) {
+                                                second_half[w_sum] = v_sum;
+                                        }
+                                } else {
+                                        second_half[w_sum] = v_sum;
+                                }
+                        }
+                }
+
+                uint64_t ans = 0;
+                for (auto fst : ms) {
+                        for (auto snd : second_half) {
+                                if (fst.first + snd.first <= w) {
+                                        uint64_t value = fst.second + snd.second;
+                                        if (ans < value) {
+                                                ans = value;
+                                        }
+                                }
+                        }
+                }
+
+                cout << ans << endl;
+        } else {
+                // all values are < 1000
+                int nvmax = 1001 * n;
+                vector<vector<uint64_t> > dp(n+1, vector<uint64_t>(nvmax, 1000000000 * n));
+                dp[0][0] = 0;
+                for (auto i=1; i<n+1; i++) {
+                        uint vi = values[i-1];
+                        uint wi = weights[i-1];
+                        for (auto j=0; j<vi; j++) {
+                                dp[i][j] = dp[i-1][j];
+                        }
+                        for (auto j=vi; j<nvmax; j++) {
+                                dp[i][j] = min(dp[i-1][j], dp[i-1][j-vi] + wi);
+                        }
+                }
+
+                uint max_v = 0;
+                for (auto i=0; i<nvmax; i++) {
+                        if(dp[n][i] <= w && max_v < i) {
+                                max_v = i;
+                        }
+                }
+
+                cout << max_v << endl;
         }
-      } else {
-        second_half[w_sum] = v_sum;
-      }
-    }
-  }
-
-  uint64_t ans = 0;
-  for (auto fst : ms) {
-    for (auto snd : second_half) {
-      if (fst.first + snd.first <= w) {
-        uint64_t value = fst.second + snd.second;
-        if (ans < value) {
-          ans = value;
-        }
-      }
-    }
-  }
-
-  cout << ans << endl;
 }
